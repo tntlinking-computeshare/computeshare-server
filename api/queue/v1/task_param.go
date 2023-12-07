5521e3b5fdb11d517f6f1eff54d8f10b8d9d1747
mohaijiang <mohaijiang110@163.com>
2023-12-07 10:04:07 +0800

    json 反序列化成指针

--- api/queue/v1/task_param.go
+++ api/queue/v1/task_param.go
@@ -12,17 +12,17 @@ func (task *QueueTaskVo) GetTaskParam() (any, error) {
 
 		var vo ComputeInstanceTaskParamVO
 		err := json.Unmarshal([]byte(task.GetParams()), &vo)
-		return vo, err
+		return &vo, err
 
 	case TaskCmd_NAT_PROXY_CREATE, TaskCmd_NAT_PROXY_DELETE,
 		TaskCmd_NAT_VISITOR_CREATE, TaskCmd_NAT_VISITOR_DELETE, TaskCmd_VM_VNC_CONNECT:
 		var vo NatNetworkMappingTaskParamVO
 		err := json.Unmarshal([]byte(task.GetParams()), &vo)
-		return vo, err
+		return &vo, err
 	case TaskCmd_STORAGE_CREATE, TaskCmd_STORAGE_DELETE:
 		var vo StorageSetupTaskParamVO
 		err := json.Unmarshal([]byte(task.GetParams()), &vo)
-		return vo, err
+		return &vo, err
 	}
 	return nil, errors.New("cannot issue command")
 }
