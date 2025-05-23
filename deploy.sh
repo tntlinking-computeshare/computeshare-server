fdfff29b2080a571669fcc20bc24de1ff434167d
mohaijiang <mohaijiang110@163.com>
2025-05-23 17:36:27 +0800

    调整目录结构

--- deploy.sh
+++ deploy.sh
@@ -8,4 +8,4 @@ export PIPELINE_ID=$(date "+%Y%m%d%H%M%S")
 docker buildx build -t registry.hamster.newtouch.com/hamstershare/computeshare-server:${PIPELINE_ID} --platform=linux/amd64 --push .
 
 ##envsubst < k8s.yml | kubectl --kubeconfig /Users/mohaijiang/.kube/config_computeshare -n computeshare apply -f -
-kubectl --kubeconfig /Users/mohaijiang/.kube/config_computeshare  -n computeshare set image deployment/computeshare-server computeshare-server=registry.hamster.newtouch.com/hamstershare/computeshare-server:${PIPELINE_ID}
+echo kubectl -n computeshare set image deployment/computeshare-server computeshare-server=registry.hamster.newtouch.com/hamstershare/computeshare-server:${PIPELINE_ID}
